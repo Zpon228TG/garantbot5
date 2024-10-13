ecd3e639871992d01d8b399be674e049f1fdab5a
Zpon228TG <daetetet11@mail.ru>
2024-10-13 18:20:28 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,14 +1,14 @@
 from pydantic_settings import BaseSettings
-from pyrogram import Client as PyrogramClient
+from pyrogram import Client
+
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import BotCommand  # Убедитесь, что импорт правильный
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-# Настройка расписателя
 scheduler = AsyncIOScheduler(timezone='Europe/Moscow', max_instances=1000)
+
 
 class Config(BaseSettings):
     TELEGRAM_API_KEY: str
@@ -28,35 +28,23 @@ class Config(BaseSettings):
 
     SCHEDULER_TASKS_INTERVAL: int
 
-# Загрузка конфигурации из .env файла
+
 config = Config(_env_file=".env", _env_file_encoding="utf-8")
 
-# Инициализация бота aiogram
 bot = Bot(token=config.TELEGRAM_API_KEY, parse_mode='HTML')
 dispatcher = Dispatcher(bot=bot, storage=MemoryStorage())
 dispatcher.middleware.setup(LoggingMiddleware())
 
-# Установка команд бота
-async def set_commands():
-    commands = [
-        BotCommand(command="/start", description="Start the bot"),
-        BotCommand(command="/help", description="Help command"),
-    ]
-    await bot.set_my_commands(commands)
+user_app = Client("my_account", api_id=config.API_ID, api_hash=config.API_HASH, )
 
-# Инициализация клиента pyrogram
-user_app = PyrogramClient("my_account", api_id=config.API_ID, api_hash=config.API_HASH)
 
-# Функция для запуска клиента pyrogram
-async def run_pyrogram_client():
+async def run_client():
     await user_app.start()
-    print("Pyrogram client started")
 
-# Основная функция для запуска
-async def main():
-    await run_pyrogram_client()
-    await set_commands()  # Устанавливаем команды после запуска клиента
-    await dispatcher.start_polling()
 
-if __name__ == '__main__':
-    asyncio.run(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run_client())
+
+bot_app = Client("my_account", api_id=config.API_ID, api_hash=config.API_HASH,
+                 bot_token=config.TELEGRAM_API_KEY,
+                 in_memory=True)
