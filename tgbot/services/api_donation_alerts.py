4c1d16c714e908ad3e8f6bc192ba4c1f167d1a46
Zpon228TG <daetetet11@mail.ru>
2024-10-20 17:39:42 +0300

    Update api_donation_alerts.py

--- tgbot/services/api_donation_alerts.py
+++ tgbot/services/api_donation_alerts.py
@@ -1,23 +1,17 @@
 import requests
-from ..bot_settings import settings  # Импортируй settings глобально
 
 class DonationAlertsAPI:
     def __init__(self, api_token: str) -> None:
         self.api_token: str = api_token
 
     async def generate_pay_link(self, amount: float):
-        """
-        Создает платежную ссылку для пополнения баланса.
-
-        :param amount: Сумма пополнения баланса в профиле.
-        :return: Платежная ссылка.
-        """
         url = "https://www.donationalerts.com/api/v1/alerts/donations"
         headers = {
             "Authorization": f"Bearer {self.api_token}"
         }
         requests.get(url=url, headers=headers)
 
-
-# Используй settings для создания экземпляра API
-API_Donation_alerts = DonationAlertsAPI(api_token=settings.DONATION_ALERTS_API_TOKEN)
+def get_donation_alerts_api():
+    # Импортируем settings только здесь, чтобы избежать циклического импорта
+    from ..bot_settings import settings
+    return DonationAlertsAPI(api_token=settings.DONATION_ALERTS_API_TOKEN)
