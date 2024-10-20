48e6e3d86ac2c5bb952e8ef268df946b51677276
Zpon228TG <daetetet11@mail.ru>
2024-10-20 17:44:44 +0300

    Update api_aaio.py

--- tgbot/services/api_aaio.py
+++ tgbot/services/api_aaio.py
@@ -1,18 +1,9 @@
-# Импорт необходимых модулей.
 import time
-
 import random
-
 import aiohttp
-
 import hashlib
-
 import secrets
 
-from ..bot_settings import settings
-
-
-# Класс работы с API платежной системой AAIO.
 class AaioAPI:
     def __init__(self, aaio_api_key, aaio_id_shop, aaio_secret_key) -> None:
         self.api_key = aaio_api_key
@@ -33,26 +24,20 @@ class AaioAPI:
         """
         async with aiohttp.ClientSession(timeout=self.timeout) as session:
             order_id = f'{time.time()}_{secrets.token_hex(random.randint(5, 10))}'
-            sign = f':'.join([str(self.shop_id), str(amount), 'RUB', str(self.secret_key), order_id])
-
-            sign = hashlib.sha256(sign.encode('utf-8')).hexdigest(),
+            sign = f'{self.shop_id}:{amount}:RUB:{self.secret_key}:{order_id}'
+            sign = hashlib.sha256(sign.encode('utf-8')).hexdigest()
 
             params = {
                 'merchant_id': self.shop_id, "amount": amount, "order_id": order_id, 'sign': sign, 'currency': 'RUB'
             }
 
             response = await session.post(url="https://aaio.io/merchant/pay", data=params)
-
-            await session.close()
-
             return str(response.url)
 
     async def get_pay_status(self, order_id):
         async with aiohttp.ClientSession(headers=self.headers, timeout=self.timeout) as session:
             params = {'order_id': order_id, 'merchant_id': self.shop_id}
             response = await session.post(url="https://aaio.io/api/info-pay", data=params)
-
-            await session.close()
             resp = await response.json()
 
             if resp['type'] == "success":
@@ -63,10 +48,13 @@ class AaioAPI:
             else:
                 return False
 
+def create_aaio_api():
+    from ..bot_settings import settings  # Импортируем настройки только здесь, чтобы избежать циклического импорта
+    return AaioAPI(
+        aaio_secret_key=settings.AAIO_SECRET_KEY,
+        aaio_id_shop=settings.AAIO_ID_SHOP,
+        aaio_api_key=settings.AAIO_API_KEY
+    )
 
-# Создание объекта класса Aaio для полноценной работы.
-API_AAIO = AaioAPI(
-    aaio_secret_key=settings.AAIO_SECRET_KEY,
-    aaio_id_shop=settings.AAIO_ID_SHOP,
-    aaio_api_key=settings.AAIO_API_KEY
-)
+# Используем функцию для создания объекта API_AAIO
+API_AAIO = create_aaio_api()
