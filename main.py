a4a718950f18e9bf3f1aecccffa89efc82965fb9
Zpon228TG <daetetet11@mail.ru>
2024-10-20 17:51:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
-
 import sys
-
-from aiogram import Dispatcher
-
+import asyncio
+from aiogram import Bot, Dispatcher
 from tgbot.bot_settings import BOT_SCHEDULER, Logger, DarkShop_bot
 from tgbot.data_base.db_helper import create_dbx
 from tgbot.middlewares import register_all_middlwares
@@ -11,15 +9,25 @@ from tgbot.routers import register_all_routers
 from tgbot.services.api_session import AsyncRequestSession
 from tgbot.utils.misc.bot_commands import set_commands
 from tgbot.utils.misc_functions import *
+from tgbot.services.api_aaio import AaioAPI
+from tgbot.bot_settings import settings, payment_systems
 
+# Инициализация API AAIO
+api_aio = AaioAPI(
+    aaio_secret_key=settings.AAIO_SECRET_KEY.get_secret_value(),
+    aaio_id_shop=settings.AAIO_ID_SHOP,
+    aaio_api_key=settings.AAIO_API_KEY.get_secret_value()
+)
+
+# Добавление API Aaio в словарь payment_systems
+payment_systems['API_Aaio'] = api_aio
 
 # Запуск планировщика.
 async def scheduler_start(bot: Bot) -> None:
-    BOT_SCHEDULER.add_job(update_profit_month, trigger="cron", day=1, hour=00, minute=00, second=5)
-    BOT_SCHEDULER.add_job(update_profit_week, trigger="cron", day_of_week="mon", hour=00, minute=00, second=10)
-    BOT_SCHEDULER.add_job(update_profit_day, trigger="cron", hour=00, minute=00, second=15, args=(bot,))
-    BOT_SCHEDULER.add_job(autobackup_admin, trigger="cron", hour=00, args=(bot,))
-
+    BOT_SCHEDULER.add_job(update_profit_month, trigger="cron", day=1, hour=0, minute=0, second=5)
+    BOT_SCHEDULER.add_job(update_profit_week, trigger="cron", day_of_week="mon", hour=0, minute=0, second=10)
+    BOT_SCHEDULER.add_job(update_profit_day, trigger="cron", hour=0, minute=0, second=15, args=(bot,))
+    BOT_SCHEDULER.add_job(autobackup_admin, trigger="cron", hour=0, args=(bot,))
 
 # Запуск бота и базовых функций
 async def main():
@@ -50,7 +58,6 @@ async def main():
     finally:
         await ar_session.close()
         await DarkShop_bot.session.close()
-
 
 if __name__ == "__main__":
     create_dbx()
